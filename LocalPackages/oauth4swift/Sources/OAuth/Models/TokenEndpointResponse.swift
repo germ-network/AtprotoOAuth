4f0baa87bb55f52ea3e0bebc954d0b160f80b14d
Mark @ Germ <mark@germ.network>
2026-03-07 09:02:25 +0800

    support bearer token, comment out OIDC id_token (still will get picked up in additional fields

--- LocalPackages/oauth4swift/Sources/OAuth/Models/TokenEndpointResponse.swift
+++ LocalPackages/oauth4swift/Sources/OAuth/Models/TokenEndpointResponse.swift
@@ -10,23 +10,28 @@ import Foundation
 public struct TokenEndpointResponse {
 	public let accessToken: String
 	public let expiresIn: Int?
-	let idToken: String?
+	//	let idToken: String?
 	public let refreshToken: String?
 	public let scope: String?
 	//	let authorizationDetails:
-	let tokenType: TokenType
+	public let tokenType: TokenType
 
 	//capture additional fields
 	let additionalFields: [String: Any]?
 
-	enum TokenType: String, Decodable {
-		//		case bearer
+	//TODO: allow extension for unknown types
+	//example in oauth4web: RecognizedTokenTypes
+	//https://github.com/panva/oauth4webapi/blob/aa0de3f77edab0f0d2b1e1f8ddf875de4f72e8e6/src/index.ts#L3211
+	public enum TokenType: String, Decodable {
+		case bearer
 		case dpop
 
 		init(string: String) throws {
 			switch string.lowercased() {
 			case TokenType.dpop.rawValue:
 				self = .dpop
+			case TokenType.bearer.rawValue:
+				self = .bearer
 			default:
 				throw OAuthError.unrecognizedTokenType
 			}
@@ -37,7 +42,7 @@ extension TokenEndpointResponse: Decodable {
 	enum CodingKeys: String, CodingKey {
 		case accessToken = "access_token"
 		case expiresIn = "expires_in"
-		case idToken = "id_token"
+		//		case idToken = "id_token"
 		case refreshToken = "refresh_token"
 		case scope
 		case tokenType = "token_type"
@@ -48,7 +53,7 @@ extension TokenEndpointResponse: Decodable {
 		let container = try decoder.container(keyedBy: CodingKeys.self)
 		accessToken = try container.decode(String.self, forKey: .accessToken)
 		expiresIn = try container.decodeIfPresent(Int.self, forKey: .expiresIn)
-		idToken = try container.decodeIfPresent(String.self, forKey: .idToken)
+		//		idToken = try container.decodeIfPresent(String.self, forKey: .idToken)
 		refreshToken = try container.decodeIfPresent(String.self, forKey: .refreshToken)
 		scope = try container.decodeIfPresent(String.self, forKey: .scope)
 		let tokenString = try container.decode(String.self, forKey: .tokenType)
