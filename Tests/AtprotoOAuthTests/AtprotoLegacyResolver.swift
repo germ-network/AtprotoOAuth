56d66d058787d31220aa4f1217fe4ed956fe6e83
Mark Xue <mark@germ.network>
2026-03-28 00:53:06 -0700

    fix tests, comment out unauth agent

--- Tests/AtprotoOAuthTests/AtprotoLegacyResolver.swift
+++ Tests/AtprotoOAuthTests/AtprotoLegacyResolver.swift
@@ -10,6 +10,7 @@ import AtprotoClient
 import AtprotoTypes
 import Foundation
 import GermConvenience
+import HTTPTypes
 
 public struct AtprotoLegacyResolver: AtprotoResolver {
 	let resourceFetcher: HTTPFetcher
@@ -32,9 +33,16 @@ public struct AtprotoLegacyResolver: AtprotoResolver {
 
 	public func resolve(did: AtprotoTypes.Atproto.DID) async throws -> Atproto.DIDDocument {
 		let url = try constructPlcQueryUrl(did: did)
-		var request = URLRequest(url: url)
-		request.addValue("application/json", forHTTPHeaderField: "Accept")
-
+		
+		let request = BundledHTTPRequest(
+			request: .init(
+				url: url,
+				headerFields: .init(
+					dictionaryLiteral: (.accept, "application/json")
+				)
+			)
+		)
+		
 		return try await resourceFetcher.data(for: request)
 			.expectSuccess()
 			.decode()
