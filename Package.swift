69d28cb76c91e7463277e15fb9990d7a5766c8a8
Anna M <anna@germ.network>
2026-04-07 12:48:16 -0700

    Rev package versions

--- Package.swift
+++ Package.swift
@@ -16,11 +16,11 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/germ-network/AtprotoClient.git",
-			branch: "mark/mockPDS"
+			from: "0.3.0"
 		),
 		.package(
 			url: "https://github.com/germ-network/AtprotoTypes.git",
-			from: "0.1.0"
+			from: "0.2.1"
 		),
 		.package(
 			url: "https://github.com/germ-network/Microcosm.git",
