ae1b92029ebdb72a0e2f0454338de311351bea7f
Anna M <anna@germ.network>
2026-04-09 12:44:13 -0700

    Update Resolver protocol functions

---
+++ .changeset/smart-corners-sneeze.md
@@ -0,0 +1,6 @@
+---
+"@germ-network/atprotooauth": minor
+---
+
+Make resolve functions have optional return values
+Add verifiedResolve (handle->didDoc) with default implementation
--- Sources/AtprotoOAuth/AtprotoResolver.swift
+++ Sources/AtprotoOAuth/AtprotoResolver.swift
@@ -9,7 +9,21 @@ import AtprotoTypes
 
 extension Atproto {
 	public protocol Resolver: Sendable {
-		func resolve(handle: AtIdentifier.Handle) async throws -> Atproto.DID
-		func resolve(did: Atproto.DID) async throws -> Atproto.DIDDocument
+		func resolve(handle: AtIdentifier.Handle) async throws -> Atproto.DID?
+		func verifiedResolve(handle: AtIdentifier.Handle) async throws -> Atproto
+			.DIDDocument?
+		func resolve(did: Atproto.DID) async throws -> Atproto.DIDDocument?
+	}
+}
+
+// Default implementation for verifiedResolve, can be overridden
+extension Atproto.Resolver {
+	public func verifiedResolve(handle: AtIdentifier.Handle) async throws -> Atproto
+		.DIDDocument?
+	{
+		guard let did = try await resolve(handle: handle) else {
+			return nil
+		}
+		return try await resolve(did: did)
 	}
 }
