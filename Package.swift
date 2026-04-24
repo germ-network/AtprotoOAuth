82d6e2ce4b18652db78bec95915212a43b3ecba9
Anna M <anna@germ.network>
2026-04-24 13:36:08 -0700

    Add block reading/writing to demo app

--- Package.swift
+++ Package.swift
@@ -16,11 +16,11 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/germ-network/AtprotoClient.git",
-			from: "0.4.0"
+			from: "0.5.2"
 		),
 		.package(
 			url: "https://github.com/germ-network/AtprotoTypes.git",
-			from: "0.2.3"
+			from: "0.3.4"
 		),
 		.package(
 			url: "https://github.com/germ-network/Microcosm.git",
