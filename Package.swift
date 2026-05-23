4db2b41a02fa5644b6cb4755329ce216958cb5ca
Mark Xue <mark@germ.network>
2026-05-23 12:18:30 -0700

    cut to released version of oauth4swift

--- Package.swift
+++ Package.swift
@@ -35,8 +35,7 @@ let package = Package(
 		),
 		.package(
 			url: "https://github.com/germ-network/oauth4swift.git",
-			//			from: "0.3.5"
-			branch: "debounce-refresh"
+			from: "0.4.0"
 		),
 		.package(
 			url: "https://github.com/apple/swift-crypto.git",
