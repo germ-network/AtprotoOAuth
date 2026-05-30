fcfdb754cad2963173ef8d8c510a3c3dc1aa094b
Anna M <anna@germ.network>
2026-05-29 17:51:54 -0700

    Add scopes to demo app

--- Demos/AtprotoApp/AtprotoOAuthDemoApp/Model/OAuthClient.swift
+++ Demos/AtprotoApp/AtprotoOAuthDemoApp/Model/OAuthClient.swift
@@ -11,10 +11,41 @@ import OAuth4Swift
 extension OAuth.ClientInfo {
 	static var demo: Self {
 		.init(
-			clientId:
-				"https://static.germnetwork.com/client-metadata.json",
-			scopes: ["atproto", "transition:generic"],
-			redirectURI: URL(string: "com.germnetwork.static:/oauth")!
+			clientId: clientIdString,
+			scopes: [
+				"atproto",
+				"rpc:app.bsky.actor.getProfile?aud=did:web:api.bsky.app#bsky_appview",
+				"rpc:app.bsky.graph.getKnownFollowers?aud=did:web:api.bsky.app#bsky_appview",
+				"include:com.germnetwork.authManageDeclaration",
+				"repo:app.bsky.graph.block?action=create&action=delete",
+			],
+			redirectURI: URL(string: redirectUriString)!
 		)
+	}
+
+	private static var clientIdString: String {
+		#if DEBUG
+			"https://beta.germdm.org/oauth-client-metadata.json"
+		#else
+			"https://germdm.app/oauth-client-metadata.json"
+		#endif
+	}
+
+	//use custom url scheme on iOS, and universal link if running iOS on Mac
+	private static var redirectUriString: String {
+		if ProcessInfo.processInfo.isiOSAppOnMac {
+			#if DEBUG
+				"https://beta.germdm.org/oauth/callback"
+			#else
+				"https://germdm.app/oauth/callback"
+			#endif
+
+		} else {
+			#if DEBUG
+				"org.germdm.beta:/callback"
+			#else
+				"app.germdm:/callback"
+			#endif
+		}
 	}
 }
